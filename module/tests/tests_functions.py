24e5632eea735f5a69f1e8c2a5d347e5f85aa324
周彦青 <zhouyanqing@zhouyanqingdeMacBook-Pro.local>
2023-10-23 01:04:57 +0200

    added oop logic,corresponding tests and example data

--- module/tests/tests_functions.py
+++ module/tests/tests_functions.py
@@ -7,20 +7,20 @@ from module.exceptions import MealTooBigError, MealNotFoundError
 
 class CaloriesCounterTestCase(TestCase):
     def test_count_meals_calories(self):
-        result = calory_counter('meal-2','meal-4','meal-6')
+        result = calory_counter(['meal-2','meal-4','meal-6'])
         self.assertEqual(result, 1115, f"Expected 1115 got {result}.")
     
     def test_count_combo_calories(self):
-        result = calory_counter('combo-1','combo-2')
+        result = calory_counter(['combo-1','combo-2'])
         self.assertEqual(result, 1770, f"Expected 1770 got {result}.")
     
     def test_count_meals_and_combos_calories(self):
-        result = calory_counter('combo-1','combo-2','meal-6')
+        result = calory_counter(['combo-1','combo-2','meal-6'])
         self.assertEqual(result, 1785, f"Expected 1785 got {result}.")
 
     def test_raise_error(self):
         with self.assertRaises(MealTooBigError) as e:
-            calory_counter('combo-1','combo-1')
+            calory_counter(['combo-1','combo-1'])
         self.assertEqual(
             e.exception.message,
             "Meal is too big! 2140 is way too much!",
@@ -28,7 +28,7 @@ class CaloriesCounterTestCase(TestCase):
         )
     def test_raise_error(self):
         with self.assertRaises(MealNotFoundError) as e:
-            calory_counter('cdfghjk')
+            calory_counter(['cdfghjk'])
         self.assertEqual(
             e.exception.message,
             "cdfghjk is not on the menu",
@@ -36,19 +36,19 @@ class CaloriesCounterTestCase(TestCase):
         )
 class PriceCounterTestCase(TestCase):
     def test_count_meals_price(self):
-        result = price_counter('meal-2','meal-4','meal-6')
+        result = price_counter(['meal-2','meal-4','meal-6'])
         self.assertEqual(result, 17, f"Expected 17 got {result}.")
     
     def test_count_combo_price(self):
-        result = price_counter('combo-1','combo-2')
+        result = price_counter(['combo-1','combo-2'])
         self.assertEqual(result, 21, f"Expected 21 got {result}.")
     
     def test_count_meals_and_combos_price(self):
-        result = price_counter('combo-1','combo-2','meal-6')
+        result = price_counter(['combo-1','combo-2','meal-6'])
         self.assertEqual(result, 25, f"Expected 25 got {result}.")
     def test_raise_error(self):
         with self.assertRaises(MealNotFoundError) as e:
-            price_counter('cdfghjk')
+            price_counter(['cdfghjk'])
         self.assertEqual(
             e.exception.message,
             "cdfghjk is not on the menu",
