d1c2455c04dbe2721510d81c0a5d1e3ab4bc76d3
wubba438 <143350279+wubba438@users.noreply.github.com>
2023-10-22 17:14:33 +0200

    feat: use more complex data (#1)
    * add a file for more complex data
    
    * third version of calory counter to process more complex data
    
    ---------
    
    Co-authored-by: 周彦青 <zhouyanqing@zhouyanqingdeMacBook-Pro.local>

--- module/calories_counter.py
+++ module/calories_counter.py
@@ -1,30 +1,14 @@
-from module.exceptions import MealTooBigError
-
-meals = {
-   'Hamburger': 600,
-   'Cheese Burger': 750,
-   'Veggie Burger': 400,
-   'Vegan Burger': 350,
-   'Sweet Potatoes': 230,
-   'Salad': 15,
-   'Iced Tea': 70,
-   'Lemonade': 90,
-}
-
-combos = {
-    "Cheesy Combo" : ["Cheese Burger", "Sweet Potatoes", "Lemonade"],
-    "Veggie Combo" : ["Veggie Burger", "Sweet Potatoes", "Iced Tea"],
-    "Vegan Combo" : ["Vegan Burger", "Salad", "Lemonade"],
-}
+from data import meals, combos
+from exceptions import MealTooBigError
 
 def calory_counter(*foods):
     total = 0
     for food in foods:
         try:
-            total += meals[food]
+            total += meals[food]['calories']
         except KeyError:
             try:
-                total += calory_counter(*combos[food])
+                total += calory_counter(*combos[food]['meals'])
             except KeyError :
                 print(f"{food} is not on the menu!")
     if total > 2000:
