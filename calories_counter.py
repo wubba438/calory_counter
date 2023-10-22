6371724daa5b318e5274c21a4deea28ba5330cdf
周彦青 <zhouyanqing@zhouyanqingdeMacBook-Pro.local>
2023-10-22 15:07:56 +0200

    version 2 colory counter including combos

--- calories_counter.py
+++ calories_counter.py
@@ -1,4 +1,4 @@
-calories = {
+meals = {
    'Hamburger': 600,
    'Cheese Burger': 750,
    'Veggie Burger': 400,
@@ -9,9 +9,22 @@ calories = {
    'Lemonade': 90,
 }
 
+combos = {
+    "Cheesy Combo" : ["Cheese Burger", "Sweet Potatoes", "Lemonade"],
+    "Veggie Combo" : ["Veggie Burger", "Sweet Potatoes", "Iced Tea"],
+    "Vegan Combo" : ["Vegan Burger", "Salad", "Lemonade"],
+}
+
 def calory_counter(*foods):
     total = 0
     for food in foods:
-        total += calories[food]
+        if food in meals:
+            total += meals[food]
+        elif food in combos:
+            for item in combos[food]:
+                total += meals[item]
+        else :
+            print("You have entered something not on the menu!")
+            return 1
     return total
 
