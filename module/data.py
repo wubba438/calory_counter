d6960d0f7024f1f651a49f39a105f19e480c0da0
周彦青 <zhouyanqing@zhouyanqingdeMacBook-Pro.local>
2023-10-22 21:35:49 +0200

    use data stored in jason files

--- module/data.py
+++ module/data.py
@@ -1,74 +1,10 @@
-meals = [
-    {
-        "id": "meal-1",
-        "name": "hamburger",
-        "calories": 600,
-        "price": 5
-    },
-    {
-        "id": "meal-2",
-        "name": "cheese burger",
-        "calories": 750,
-        "price": 7
-		},
-    {
-        "id": "meal-3",
-        "name": "veggie burger",
-        "calories": 400,
-        "price": 6
-		},
-    {
-        "id": "meal-4",
-        "name": "vegan burger",
-        "calories": 350,
-        "price": 6
-		},
-    {
-        "id": "meal-5",
-        "name": "sweet potatoes",
-        "calories": 230,
-        "price": 3
-		},
-    {
-        "id": "meal-6",
-        "name": "salad",
-        "calories": 15,
-        "price": 4
-		},
-    {
-        "id": "meal-7",
-        "name": "iced tea",
-        "calories": 70,
-        "price": 2
-		},
-    {
-        "id": "meal-8",
-        "name": "lemonade",
-        "calories": 90,
-        "price": 2
-		}
-]
+import json
 
-combos = [
-    {
-        "id": "combo-1",
-        "name": "cheesy combo",
-        "meals": ["meal-2", "meal-5", "meal-8"],
-        "price": 11,
-    },
-    {
-        "id": "combo-2",
-        "name": "veggie combo",
-        "meals": ["meal-3", "meal-5", "meal-7"],
-        "price": 10,
-    },
-    {
-        "id": "combo-3",
-        "name": "vegan combo",
-        "meals": ["meal-4", "meal-6", "meal-8"],
-        "price": 10,
-    }
-]
+with open("module/data/meals.json") as file:
+	meals = json.load(file)['meals']
+with open("module/data/combos.json") as file:
+	combos = json.load(file)['combos']
+print(meals)
 
 meals = {meal['id']: meal for meal in meals}
 combos = {combo['id']: combo for combo in combos}
