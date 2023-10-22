d32d0d8340154b1921a3bca2450cd96168146a46
周彦青 <zhouyanqing@zhouyanqingdeMacBook-Pro.local>
2023-10-22 14:58:09 +0200

    original version of calory counter

---
+++ calories_counter.py
@@ -0,0 +1,17 @@
+calories = {
+   'Hamburger': 600,
+   'Cheese Burger': 750,
+   'Veggie Burger': 400,
+   'Vegan Burger': 350,
+   'Sweet Potatoes': 230,
+   'Salad': 15,
+   'Iced Tea': 70,
+   'Lemonade': 90,
+}
+
+def calory_counter(*foods):
+    total = 0
+    for food in foods:
+        total += calories[food]
+    return total
+
